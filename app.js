88ce6811aa3d8c155f3e0f398b0b2db1f247e654
Andrew <andrewbowden92@gmail.com>
2018-09-05 11:42:23 -0700

    New Calculator Vanilla JS

---
+++ app.js
@@ -0,0 +1,18 @@
+var firstNumber = 0;
+var secondNumber = 0;
+var operator;
+var equals;
+var numberChosen;
+var submit;
+var user;
+
+
+//operator math
+
+function doMath() {
+    
+}
+
+document.getElementsByClassName("button").addEventListener("click", doMath()); {
+
+}
